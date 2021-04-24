87367f14655dd992538db9cc0106f04b74d5fa98
kofithebass <kofi@kofihayford.com>
2021-04-23 21:49:28 -0400

    Removed unwind to allow array to operate normally. It was causing issues with the dashboard. Removed unnecessary data from apiroutes.js to only include totalDuration and totalWeight. The app is working now. Cleaned up data that wasn't looking great. Graphs, charts etc are updating. All is in order.

---
+++ Fitness Tracker README.md
@@ -0,0 +1,38 @@
+
+#Kofi's Fitness Tracker   
+
+## Description
+
+It is an app to help users that want to track their workouts do so with ease and see how much cumulative work they are doing over time.
+
+## Table of Contents 
+
+The package.json, index.js, and other basics. Run npm i to get the crucial pieces installed to run the app.   
+
+## Installation Instructions 
+
+npm i 
+
+## Usage 
+
+To help users make progress in their workout regimes. By tracking both their cardio and resistance workouts over time, they can make faster progress towards their fitness goals. For instance, you will more likely get to the "1,000 lb" club by working at it consistently. You can track your progress over time using this app
+
+## License 
+
+Not applicable
+
+## Contribution 
+
+All the routes, models, and basic modifications to the given code to allow it to work. Used moment.js to get time/date information. Used Swagger to test the GET, PUT, and POST requests to ensure data was making it across. 
+
+### Testing 
+
+Many tests using console.log on the front and back-end. Also used Swagger to test  GET, PUT, and POST requests.
+
+### Question 
+
+However the user sees fit
+
+
+    
+    
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -33,24 +33,48 @@ router.put("/workouts/:id", (req, res) => {
         })
 })
 
-router.get("/workouts/range", (req, res) => {
-    // Workout.aggregate([
-    //     //use unwind to destructure the array so we can extract and then use dot notation to grab the actual items from the array. 
-    //     { "$unwind": "$exercises" },
-    //     {
-    //         $group: {
-    //             _id: null,
-    //             totalDistance: { $sum: "$exercises.distance" },
-    //             totalDuration: { $sum: "$exercises.duration" },
-    //             totalWeight: { $sum: "$exercises.weight" },
-    //             totalSets: { $sum: "$exercises.sets" },
-    //             totalReps: { $sum: "$exercises.reps" },
-    //             numExercises: { $sum: "$exercises." },
-    //         }
-    //     }
+router.post("/workouts/", (req, res) => {
+    console.log(req.body, 'this our frontend body')
+    //var 
+    let testData = [req.body]
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
 
-    // ])
-    Workout.find({})
+    let newDate = new Date()
+
+    console.log(newDate)
+
+    Workout.create({ exercises: testData, day: newDate })
+        /*
+        
+    
+        */
+        .then(results => {
+            // removing anything from arr 
+            // putting req.body into new arr 
+
+            res.json(results)
+        }).catch(err => {
+            console.log(err)
+        })
+})
+
+router.get("/workouts/range", (req, res) => {
+    Workout.aggregate([
+        //use unwind to destructure the array so we can extract and then use dot notation to grab the actual items from the array. 
+
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+                totalWeight: { $sum: "$exercises.weight" },
+            }
+        }
+
+    ])
         .then(results => {
             console.log(results, 'this is our results incoming ')
             res.json(results)
